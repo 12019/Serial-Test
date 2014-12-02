e0ab312a17a3551dd84471d6c36610bd19ee71ee
mudkipmaster <acg59@drexel.edu>
2014-12-01 20:42:45 -0500

    Added Kinect libraries

---
+++ SkeletalViewer/BodyDetection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Microsoft.Research.Kinect.Nui;
+
+namespace SkeletalViewer
+{
+    class BodyDetection
+    {
+
+    }
+}
